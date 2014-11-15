fddac66022b57237f13b41910a38294cd4688b0c
Rahul Krishnamurthy <rahul013k@gmail.com>
2014-11-15 10:00:21 -0600

    Updated Catalog - Check if it is fine, if yes i'll continue

---
+++ WEB-INF/classes/beans/ProductData.java
@@ -0,0 +1,73 @@
+package beans;
+
+import java.io.*;
+import java.util.*;
+public class ProductData{
+
+	private String prodid;
+	private String category;
+	private String description;
+	private String country;
+	private double price;
+
+	public ProductData(String prodid, String category, String description, String country, double cost){
+
+
+		setProdId(prodid);
+		setCategory(category);
+		setDecription(description);
+		setCountry(country);
+		setPrice(cost);
+	}
+
+
+	public void setProdId(String prodid){
+
+		this.prodid = prodid;
+	}
+
+	public String getProdId(){
+
+		return prodid;
+	}
+
+	public void setCategory(String category){
+		this.category = category;
+	}
+
+	public String getCategory(){
+
+		return category;
+	}
+	public void setDecription(String desc){
+		this.description = description;
+
+	}
+
+	public String getDescription(){
+
+		return description;
+	}
+
+	public void setCountry(String country){
+		this.country = country;
+	}
+
+	public String getCountry(){
+
+		return country;
+	}
+
+	public void setPrice(double price){
+
+		this.price = price;
+	}
+
+	public double getPrice(){
+
+		return price;
+	}
+
+	
+	
+}
